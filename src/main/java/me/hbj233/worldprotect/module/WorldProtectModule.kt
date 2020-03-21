b0ec3d005cc0cbc6c1582de6679a8a42e191b6b3
233hbj <8mpkH!8W&O9pnp0!>
2020-03-21 13:15:11 +0800

    Update version to 1.1.

--- src/main/java/me/hbj233/worldprotect/module/WorldProtectModule.kt
+++ src/main/java/me/hbj233/worldprotect/module/WorldProtectModule.kt
@@ -24,7 +24,7 @@ object WorldProtectModule : SimpleEasyAPIModule() {
             WorldProtectPlugin.instance,
             MODULE_NAME,
             AUTHOR,
-            ModuleVersion(1, 0, 0)
+            ModuleVersion(1, 1, 0)
     )
 
     override fun moduleRegister() {
